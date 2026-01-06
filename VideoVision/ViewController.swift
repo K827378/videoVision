3dde87087f90c0db43cce0cb293fb8f82c907595
Rinkim0515 <kimrindev@gmail.com>
2026-01-06 23:36:32 +0900

    init project

---
+++ VideoVision/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoVision
+//
+//  Created by KimRin on 1/6/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
